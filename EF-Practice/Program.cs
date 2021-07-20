6f422c76b80318415497c500f5ad3439d13372fc
Zach McWhirter <zach@healthlock.com>
2021-07-20 14:37:23 -0500

    Add project files.

---
+++ EF-Practice/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EF_Practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
