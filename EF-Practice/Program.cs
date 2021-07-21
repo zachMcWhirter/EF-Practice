25c9f1855187feab2fd6fdaf5f75c6d4a05c06b6
Zach McWhirter <zach@healthlock.com>
2021-07-21 16:02:57 -0500

    execute Reverse Engineer

--- EF-Practice/Program.cs
+++ EF-Practice/Program.cs
@@ -1,12 +1,52 @@
-﻿using System;
+﻿using EF_Practice.Models;
+using System;
+using System.Linq;
 
-namespace EF_Practice
+using var db = new EfpracticedbContext();
+
+var familyMembers = db.FamilyMembers
+    .OrderBy(i => i.Id)
+    .Take(6)
+    .ToList();
+    
+Console.WriteLine("  OUR FAMILY MEMBERS");
+Console.WriteLine("  ------------------");
+
+foreach (var item in familyMembers)
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
+    Console.WriteLine($"  {item.FirstName}"); 
 }
+
+var friends = db.Friends
+    .OrderBy(i => i.Id)
+    .Take(6)
+    .ToList();
+
+Console.WriteLine("");
+Console.WriteLine("  OUR FRIENDS");
+Console.WriteLine("  -----------");
+
+foreach (var item in friends)
+{
+    Console.WriteLine($"  {item.FirstName} is {item.ConnectedTo}'s friend");
+    Console.WriteLine();
+}
+
+
+//var orders = await db.GetProcedures().CustOrderHist("ALFKI");
+//foreach (var order in orders)
+//    Console.WriteLine($"{order.ProductName}: {order.Total}");
+
+//var outOverallCount = new OutputParameter<int?>();
+//var customers = await db.GetProcedures().SP_GET_TOP_IDS(10, outOverallCount);
+//Console.WriteLine($"Db contains {outOverallCount.Value} Customers.");
+//foreach (var customer in customers)
+//    Console.WriteLine(customer.CustomerId);
+
+// Use scalar functions inline in queries:
+
+
+//Where(o => o.Reference == EfpracticedbContext.GetReference(o.OrderName)).ToList();
+
+//    var tvfTest = db.ProductsWithMinimumInStock(5)
+
